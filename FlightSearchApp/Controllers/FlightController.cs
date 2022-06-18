20ead882bad096b1194600ad63306249fba15661
Harsh Dave <harshdave71720@gmail.com>
2022-06-18 10:54:05 +0530

    Add project files.

---
+++ FlightSearchApp/Controllers/FlightController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightSearchApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightController : ControllerBase
+    {
+        public IActionResult UploadProviderFile()
+        {
+            return Ok();
+        }
+    }
+}
